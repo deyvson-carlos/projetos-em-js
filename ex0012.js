ab05cc2b97105137f1f5bacb09b713e7855f78fe
Deyvson-Silva <deyvsoncarlos92@gmail.com>
2022-05-18 22:31:29 -0300

    first commit

---
+++ ex0012.js
@@ -0,0 +1,10 @@
+var país = 'frança'
+console.log(`Vivendo em ${país}`)
+
+if(país == 'brasil') {
+    console.log('Você é Brasileiro!')
+}
+
+else{
+    console.log('Você é estrangeiro!')
+}
---
+++ ex0017.js
@@ -0,0 +1,27 @@
+function carregar() {
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+var hora = data.getHours()
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >= 0 && hora < 12){
+    //Bom dia
+    img.src = 'manha (1).jpg'
+    document.body.style.background = '#e2cd9f'
+}
+
+else if (hora >= 12 && hor <18){
+    //Boa Tarde
+    img.src = 'tarde.jpg'
+    document.body.style.background = '#b98446f'
+}
+
+else{
+    //Boa noite
+    img.src = 'noite.jpg'
+    document.body.style.background = '#515154'
+}
+
+}
